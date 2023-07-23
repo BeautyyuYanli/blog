9469a52754828060b57c017558c80d7dafc2b630
BeautyyuYanli <beautyyuyanli@gmail.com>
2023-07-24 00:49:40 +0800

    use GA

--- blog.config.js
+++ blog.config.js
@@ -27,14 +27,14 @@ const BLOG = {
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
-    provider: 'ackee', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
+    provider: 'ga', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
     ackeeConfig: {
       tracker: 'https://ackee.beautyyu.one/tracker.js', // e.g 'https://ackee.craigary.net/tracker.js'
       dataAckeeServer: 'https://ackee.beautyyu.one', // e.g https://ackee.craigary.net , don't end with a slash
       domainId: '87a4163e-1dcf-414f-804c-70be90ef6196' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: '' // e.g: G-XXXXXXXXXX
+      measurementId: 'G-WTVFETW1S2' // e.g: G-XXXXXXXXXX
     }
   },
   comment: {
